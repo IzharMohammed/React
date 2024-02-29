7ac180dcaaed30f289d1af22d3710857180e6794
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-02-29 11:52:58 +0530

    Added context api with chai aur code

--- contextApi/src/App.jsx
+++ contextApi/src/App.jsx
@@ -1,12 +1,15 @@
-
-
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/userContextProvider";
 function App() {
-
   return (
-<>
-
-</>
-  )
+    <>
+      <UserContextProvider>
+        <Login />
+        <Profile />
+      </UserContextProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
