0678d6a4413a347d018d2c815d4c82a5d6980550
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-02-29 18:11:35 +0530

    Added theme changer as a project of context Api(another method)

---
+++ themeSwitcher/src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+import { ThemeContextProvider } from "./contexts/theme";
+import { useEffect, useState } from "react";
+function App() {
+  const [themeMode, setthemeMode] = useState("light");
+  const lightTheme = () => {
+    setthemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setthemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeContextProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeContextProvider>
+  );
+}
+
+export default App;
